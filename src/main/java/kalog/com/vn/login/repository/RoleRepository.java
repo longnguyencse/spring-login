a4293225df5e893ac09845c1c9a74f1a4de8c936
nqlong <longnguyenqu@gmail.com>
2019-01-06 22:50:49 +0700

    init

---
+++ src/main/java/kalog/com/vn/login/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package kalog.com.vn.login.repository;
+
+import kalog.com.vn.login.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+}
---
+++ src/main/java/kalog/com/vn/login/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package kalog.com.vn.login.repository;
+
+import kalog.com.vn.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
